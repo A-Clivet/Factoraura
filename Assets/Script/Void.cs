f618b56b536e3fe5127f7d91b04bc89b52c2f272
A-Clivet <a.clivet@lahorde.school>
2023-10-30 12:05:58 +0100

    update

---
+++ Assets/Script/Void.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Void : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
+    }
+}
