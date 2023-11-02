58c316287d52a383c52fd20be594d9a2f641e85f
A-Clivet <a.clivet@lahorde.school>
2023-11-02 15:25:24 +0100

    UseMoney 2
    affichage du bon prix sur les boutons des objets

---
+++ Assets/Script/CostPrint.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CostPrint : MonoBehaviour
+{
+    [SerializeField] GameObject machine;
+    [SerializeField] TextMeshProUGUI m_TextMeshProUGUI;
+    int cost = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cost = PriceManager.Instance.Cost(machine);
+        m_TextMeshProUGUI.text = "Prix : " + cost;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
